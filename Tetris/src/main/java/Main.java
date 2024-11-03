1091fbd48465df7cac3c76d68799196e7c9f3250
NateDev5 <123912175+NateDev5@users.noreply.github.com>
2024-11-02 23:29:30 -0400

    d

--- Tetris/src/main/java/Main.java
+++ Tetris/src/main/java/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            Tetris tetris = new Tetris();
-            tetris.startGame();
-        }
-        catch (Exception e) {
-            throw e;
-        }
+        Tetris tetris = new Tetris();
+        tetris.startGame();
     }
 }
